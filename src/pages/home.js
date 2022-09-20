3916096d34bbefccc32b5874fc3431972b2893a0
aprilslis <s826li@uwaterloo.ca>
2022-09-20 17:36:05 -0400

    change name font

--- src/pages/home.js
+++ src/pages/home.js
@@ -16,12 +16,12 @@ function Home() {
       <Container fluid={true}>
         <Row style={{ height:"200vh", backgroundImage:`url(${topBackground})`,backgroundSize: "100% auto", backgroundRepeat: "no-repeat"}}>
           <Col sm={2}></Col>
-          <Col sm={8} style={{backgroundImage:`url(${circleRim2})`, backgroundPosition: "center", backgroundSize: "auto 100%", height:"100vh"}}>
+          <Col sm={8} style={{backgroundImage:`url(${circleRim2})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat", height:"100vh"}}>
             <Row style={{height:"20vh"}}></Row>
             <Row style={{height:"60vh"}}>
-              <label style={{margin:"auto", marginBottom:"10px", color:"#605448", fontFamily:"DMSerif", fontWeight: "100", fontSize: "650%"}}>APRIL LI</label>
+              <label style={{margin:"auto", color:"#605448", fontFamily:"DMSerif", fontWeight: "100", fontSize: "650%"}}>APRIL LI</label>
               <div style={{marginLeft:"auto", marginRight:"auto", backgroundColor:"#605448", height:"3px", width:"50%"}}/>
-              <label style={{marginTop:"0px", color:"#605448", fontFamily:"DMSerif", fontWeight: "100", fontSize: "270%"}}>Welcome to my website!</label>
+              <label style={{marginTop:"0px", color:"#605448", fontFamily:"DMSerif", fontWeight: "100", fontSize: "250%"}}>Welcome to my website!</label>
             </Row>
 
             <Row style={{height:"20vh"}}></Row>

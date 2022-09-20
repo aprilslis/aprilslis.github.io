5aef947b032d6e190cc266bd44e5a5205b7d367c
aprilslis <s826li@uwaterloo.ca>
2022-09-20 18:08:02 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, HashRouter } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,34 +12,34 @@ import Project from './pages/project';
 
 function App() {
   return (
-    <div className="App" style={{backgroundColor:"#a8d1e5"}}>
-      <div style={{backgroundColor:"#edb3af", height:"2vh"}}/>
+    <div className="App" style={{ backgroundColor: "#a8d1e5" }}>
+      <div style={{ backgroundColor: "#edb3af", height: "2vh" }} />
       <Nav
-      activeKey="/"
+        activeKey="/"
       // onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item className="nav-font">
-        <Nav.Link style={{color:"#605448", fontFamily:"DMSerif", fontWeight: "300", fontSize: "120%"}} href="/">HOME</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className="nav-font">
-        <Nav.Link style={{color:"#605448", fontFamily:"DMSerif", fontWeight: "300", fontSize: "120%"}} href="/project">Projects</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className="nav-font">
-        <Nav.Link style={{color:"#605448", fontFamily:"DMSerif", fontWeight: "300", fontSize: "120%"}} href="/exp">Work</Nav.Link>
-      </Nav.Item>
-      {/* <Nav.Item className="nav-font">
+      >
+        <Nav.Item className="nav-font">
+          <Nav.Link style={{ color: "#605448", fontFamily: "DMSerif", fontWeight: "300", fontSize: "120%" }} href="/">HOME</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-font">
+          <Nav.Link style={{ color: "#605448", fontFamily: "DMSerif", fontWeight: "300", fontSize: "120%" }} href="/project">Projects</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-font">
+          <Nav.Link style={{ color: "#605448", fontFamily: "DMSerif", fontWeight: "300", fontSize: "120%" }} href="/exp">Work</Nav.Link>
+        </Nav.Item>
+        {/* <Nav.Item className="nav-font">
         <Nav.Link style={{color:"#605448", fontFamily:"DMSerif", fontWeight: "300", fontSize: "120%"}} eventKey="disabled" disabled>
           Disabled
         </Nav.Link>
       </Nav.Item> */}
-    </Nav>
+      </Nav>
 
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project" element={<Project />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter basename={window.location.pathname || ''}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/project" element={<Project />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }

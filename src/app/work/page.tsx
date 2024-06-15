a9e64f78e9aa336f877b8032da119fe0ccea12f9
aprilslis <aaprilli.sy@gmail.com>
2024-06-15 13:14:05 +0200

    Update job experience

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
 import styles from "./page.module.scss";
+import chat from "@/../public/chat.png";
 import chrome from "@/../public/chrome.png";
 import cruise from "@/../public/cruise.png";
 import cae from "@/../public/cae.jpeg";
@@ -11,34 +12,48 @@ export default function Home() {
             <Header />
             <div className={styles.body}>
                 <div className={styles.experience}>Work Experience</div>
-                <Job
-                    logo={chrome}
-                    company="Google"
-                    position="Software Engineer Intern"
-                    description="Researched efficiency improvements for Chrome's 2D Graphic Library SKIA."
-                    divider
-                />
-                <Job
-                    logo={cruise}
-                    company="Cruise Automation"
-                    position="Software Engineer Intern"
-                    description="Member of the Remote Assitance Team, assisting vehicles that get stuck."
-                    divider
-                />
-                <Job
-                    logo={dino}
-                    company="Google"
-                    position="STEP Intern"
-                    description="Built metrics to analyze website performance on Chrome."
-                    divider
-                />
-                <Job
-                    logo={cae}
-                    company="CAE Aviation"
-                    position="Cybersecurity Development Intern"
-                    description="Created internal tooling for pre-release security checks."
-                    divider
-                />
+                <div className={styles.experienceBox}>
+                    <Job
+                        logo={chat}
+                        company="Google"
+                        position="Software Engineer Intern"
+                        description="Developing machine learning features for Google Chat."
+                        timeline="Jul 2024 - Sep 2024"
+                        divider
+                    />
+                    <Job
+                        logo={chrome}
+                        company="Google"
+                        position="Software Engineer Intern"
+                        description="Researched efficiency improvements for Chrome's 2D Graphic Library SKIA."
+                        timeline="May 2023 - Aug 2023"
+                        divider
+                    />
+                    <Job
+                        logo={cruise}
+                        company="Cruise Automation"
+                        position="Software Engineer Intern"
+                        description="Member of the Remote Assitance Team, assisting vehicles that get stuck."
+                        timeline="Jan 2023 - Apr 2023"
+                        divider
+                    />
+                    <Job
+                        logo={dino}
+                        company="Google"
+                        position="STEP Intern"
+                        description="Built metrics to analyze website performance on Chrome."
+                        timeline="May 2022 - Aug 2022"
+                        divider
+                    />
+                    <Job
+                        logo={cae}
+                        company="CAE Aviation"
+                        position="Cybersecurity Development Intern"
+                        description="Created internal tooling for pre-release security checks."
+                        timeline="Sep 2021 - Dec 2021"
+                        divider
+                    />
+                </div>
             </div>
         </div>
     );
@@ -49,6 +64,7 @@ interface JobProps {
     company: string;
     position: string;
     description: string; // could make this an array so that it make bullet pts
+    timeline: string;
     divider?: boolean;
 }
 
@@ -57,6 +73,7 @@ function Job({
     company,
     position,
     description,
+    timeline,
     divider = false,
 }: JobProps) {
     return (
@@ -65,16 +82,17 @@ function Job({
             <div className={styles.title}>
                 <img className={styles.logo} src={logo.src} />
                 <div>
-                    <label>
-                        <b>{company}</b>
+                    <label className={styles.jobInfo}>
                         <div className={styles.position}>
+                            <b>{company}</b>
                             <b>{position}</b>
                         </div>
+                        <div className={styles.date}>{timeline}</div>
+                        <div className={styles.description}>{description}</div>
                     </label>
                 </div>
             </div>
 
-            <div className={styles.description}>{description}</div>
         </div>
     );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@ import styles from "./header.module.scss";
 
 export default function Header() {
     return (
-        <div className={styles["body"]} style={{ backgroundColor: "#a8d1e5" }}>
-            <div style={{ backgroundColor: "#edb3af", height: "2vh" }} />
+        <div className={styles["body"]}>
+            <div className={styles.top} />
             <div className={styles.nav}>
                 <div className={styles["nav-font"]}>
                     <a className={styles.link} href="/">
